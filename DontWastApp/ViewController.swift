23a8941866b04d454144d10f1d88bfef5c9237f7
Everton <evtdias_@hotmail.com>
2021-08-10 16:37:28 -0300

    Initial Commit

---
+++ DontWastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DontWastApp
+//
+//  Created by user on 10/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
